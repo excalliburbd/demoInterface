47d6ef8853c14fb0a938739ec5d05114484a8148
excalliburbd <excalliburbd@gmail.com>
2016-06-03 21:23:59 +0600

    Finally got the interactive lcd to work!

--- client/main.js
+++ client/main.js
@@ -1,22 +1,19 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+  'click .blink'(event) {
+    Meteor.call('led.blink');
+    Meteor.call('lcd.write',"fsfsdfdfsfsdfsj");
   },
+  'click .off'(event) {
+    Meteor.call('led.off');
+  },
+  'keyup input' ( event ) {
+    let value = event.target.value;
+    Meteor.call('lcd.write',value.toString());
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,18 +1,75 @@
 import { Meteor } from 'meteor/meteor';
-import * as JohnnyFive from 'johnny-five';
+import * as five from 'johnny-five';
 
-Meteor.startup(() => {
+/*Meteor.startup(() => {
     board = new JohnnyFive.Board();
 
     board.on('error', function (error) {
         console.error('Johnny Five Error', error);
     });
+      led = new JohnnyFive.Led(13);
 
-    board.on("ready", Meteor.bindEnvironment(function() {
-
-        var led = new JohnnyFive.Led(13);
-
+});
+Meteor.methods({
+    'led.blink'(){
         led.blink(500);
+    },
+    'led.toggle'(){
+      led.toggle();
+    }
+});*/
 
-    }, "ready"));
+Meteor.startup(() => {
+    board = new five.Board();
+
+    board.on("ready", function() {
+
+      lcd = new five.LCD({
+        // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+        // Arduino pin # 7    8   9   10  11  12
+        pins: [7, 8, 9, 10, 11, 12],
+        backlight: 6,
+        rows: 2,
+        cols: 12
+        // Options:
+        // bitMode: 4 or 8, defaults to 4
+        // lines: number of lines, defaults to 2
+        // dots: matrix dimensions, defaults to "5x8"
+      });
+      // Tell the LCD you will use these characters:
+      lcd.useChar("check");
+      lcd.useChar("heart");
+      lcd.useChar("duck");
+      /*//Line 1: Hi rmurphey & hgstrp!
+      lcd.clear().print("rmurphey & hgstrp!");
+      lcd.cursor(1, 0);
+
+      //Line 2: I <3 johnny-five
+      //cd.print("I").write(7).print(" johnny-five");
+      // can now be written as:
+      lcd.print("I :heart: johnny-five");
+      */
+      /*this.wait(3000, function() {
+        lcd.clear().cursor(0, 0).print(":heart: 2 :duck: :)");
+      });*/
+      this.repl.inject({
+        lcd: lcd
+      });
+    });
+})
+
+Meteor.methods({
+  'lcd.write'(message){
+    if(message.length <= 16){
+      lcd.cursor(0, 0);
+      lcd.clear().print(message);
+    }else{
+      lcd.cursor(0, 0);
+      lcd.clear();
+      let msg = message.slice(0,16);
+      lcd.print(msg);
+      lcd.cursor(1, 0);
+      lcd.print(message.slice(16));
+    }
+  }
 });
